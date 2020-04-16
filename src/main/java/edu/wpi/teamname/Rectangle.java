f9ffb94c033ed06ef544db83174b489518a9b7e9
npelletier2 <npelletier@wpi.edu>
2020-04-16 15:39:38 -0400

    Circle tests

--- src/main/java/edu/wpi/teamname/Rectangle.java
+++ src/main/java/edu/wpi/teamname/Rectangle.java
@@ -1,27 +1,27 @@
 package edu.wpi.teamname;
 
 public class Rectangle {
-    private double height;
-    private double width;
+  private double height;
+  private double width;
 
-    public Rectangle(double h, double w){
-        height = h;
-        width = w;
-    }
+  public Rectangle(double h, double w) {
+    height = h;
+    width = w;
+  }
 
-    public double getHeight() {
-        return height;
-    }
+  public double getHeight() {
+    return height;
+  }
 
-    public double getWidth() {
-        return width;
-    }
+  public double getWidth() {
+    return width;
+  }
 
-    public double getPerimeter(){
-        return (2*height) + (2*width);
-    }
+  public double getPerimeter() {
+    return (2 * height) + (2 * width);
+  }
 
-    public double getArea(){
-        return height*width;
-    }
+  public double getArea() {
+    return height * width;
+  }
 }
--- src/test/java/edu/wpi/teamname/GeoTest.java
+++ src/test/java/edu/wpi/teamname/GeoTest.java
@@ -1,19 +1,31 @@
 package edu.wpi.teamname;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 
 public class GeoTest {
-    @Test
-    public void testPerimeter(){
-        Rectangle rect = new Rectangle(3,4);
-        assertTrue(rect.getPerimeter() == (rect.getHeight()*2)+(rect.getWidth()*2));
-    }
-    @Test
-    public void testArea(){
-        Rectangle rect = new Rectangle(3,4);
-        assertTrue(rect.getArea() == (rect.getHeight()*rect.getWidth()));
-    }
+  @Test
+  public void testPerimeter() {
+    Rectangle rect = new Rectangle(3, 4);
+    assertTrue(rect.getPerimeter() == (rect.getHeight() * 2) + (rect.getWidth() * 2));
+  }
+
+  @Test
+  public void testArea() {
+    Rectangle rect = new Rectangle(3, 4);
+    assertTrue(rect.getArea() == (rect.getHeight() * rect.getWidth()));
+  }
+
+  @Test
+  public void testCircleArea(){
+    Circle circ = new Circle(3);
+    assertEquals(3*3*Math.PI, circ.getArea());
+  }
+
+  @Test
+  public void testCirclePerimeter(){
+    Circle circ = new Circle(3);
+    assertEquals(6*Math.PI, circ.getPerimeter());
+  }
 }
